98e344eb18f8910cdfdcee90827d58645cc91060
dreamlineproduction <info@dreamlineproduction.in>
2021-02-01 18:36:19 +0530

    Working on Profile page

--- user-profile.php
+++ user-profile.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Imagn</title>
+    <title>Dev Chatterjee - Imagn</title>
     <link href="fucking-css/bootstrap.css" rel="stylesheet">
     <link href="fucking-css/main.css" rel="stylesheet">
     <link rel="stylesheet" href="webfonts/stylesheet.css" type="text/css" charset="utf-8" />
@@ -15,68 +15,203 @@
 
 <body>
 
-<?php include 'include/navbar2.php';?>
-   <div class="supreme-container">
-   <div class="container-fluid p-0">
-       <div class="profile-head-image">
-   <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="fucking-images/head-banner.jpg" class="d-block w-100" alt="...">
+    <?php include 'include/navbar2.php';?>
+    <div class="supreme-container">
+        <div class="container-fluid p-0">
+            <div class="profile-head-image">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="fucking-images/head-banner.jpg" class="d-block w-100" alt="...">
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+        <div class="container">
+            <div class="profile-card-top-section">
+                <div class="row">
+                    <div class="user-profile-avatar d-flex justify-content-center">
+                        <div class="profile-img-src"
+                            style="background-image: url(fucking-images/dev-chatterjee-seo-expert.jpg);"></div>
+                        <!-- <img src="fucking-images/dev-chatterjee-seo-expert.jpg"  class="img-fluid img-thumbnail" alt="..."> -->
+
+                    </div>
+                </div>
+
+                <div class="user-top-information">
+                    <div class="row">
+                        <div class="col-md-4 user-top-left-side-widget border-end px-4">
+                            <h3>My Money, My Income</h3>
+                            <h5>Inbound Medic <small>(Founder)</small></h5>
+                            <a href="www.inboundmedic.com" target="_blank"><i class="fas fa-external-link-alt"></i>
+                                www.inboundmedic.com</a>
+                            <p>I help elite medical practices get more new patients through Google.</p>
+                            <a class="btn btn-primary user-top-left-side-widget-btn" href="#" role="button">Schedule A
+                                Strategy Session</a>
+                        </div>
+
+                        <div class="col-md-4 user-top-center-side-widget border-end text-center px-4">
+                            <h3>Dev Chatterjee</h3>
+                            <span>Artist</span> | <span>Entrepreneur</span> | <span>Fool
+                                Creating The Next Social Network
+                                And Pissing Off The Entire World</span>
+
+                            <div class="user-hobby-section">
+
+                                <p> <i class="fas fa-home"></i> Lives In: New York, NY</p>
+
+                                <p> <i class="fas fa-guitar"></i> Jazz Guitar, Pool, Video Gaming</p>
+
+                            </div>
+
+
+                        </div>
+
+                        <div class="col-md-4 user-top-left-side-widget px-4">
+                            <h3>My Meaning, My Impact</h3>
+                            <h5>Imagn.World <small>(Founder)</small></h5>
+                            <a href="www.imagn.world" target="_blank"><i class="fas fa-external-link-alt"></i>
+                                www.imagn.world</a>
+                            <p>A new world order to celebrate the political and economic paradigm of the Artist's Dream.
+                            </p>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <section>
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-12 text-center my-consciousness">
+                        <h1>My Consciousness</h1>
+                    </div>
+                </div>
+                <div class="row mt-5">
+
+                    <div class="col-md-8 p-0">
+                        <div class="my-consciousness-top-nav">
+                            <div class="card bg-light-black rounded-0 text-white">
+                                <div class="card-body my-consciousness-menu-top">
+
+
+                                    <div class="scroller scroller-left"><i class="fas fa-chevron-left"></i></div>
+                                    <div class="scroller scroller-right"><i class="fas fa-chevron-right"></i></div>
+                                    <div class="wrapper">
+                                        <ul class="nav list" id="myTab">
+                                            <li class="active"><a href="#home">My Journal</a></li>
+                                            <li><a href="#profile">My Politics</a></li>
+                                            <li><a href="#messages">Photos</a></li>
+                                            <li><a href="#settings">Videos</a></li>
+                                            <li><a href="#">Events</a></li>
+                                            <li><a href="#">Audio</a></li>
+                                            <li><a href="#">Poll</a></li>
+                                            <li><a href="#">Podcast</a></li>
+                                            <li><a href="#settings">Videos</a></li>
+                                            <li><a href="#">Events</a></li>
+                                            <li><a href="#">Audio</a></li>
+                                            <li><a href="#">Poll</a></li>
+                                            <li><a href="#">Podcast</a></li>
+
+                                        </ul>
+                                    </div>
+
+
+
+
+
+
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-4 p-0">
+                        <div class="my-consciousness-left-section bg-black text-white">
+                            <div class="widget-1">
+                                <div class="widget-title">
+                                    <h1 class="border-bottom">My Identity Inspiration </h1>
+                                </div>
+                                <div class="edit-widget-btn">
+                                    <a href="#"> <span><i class="far fa-edit"></i></span> </a>
+                                </div>
+                                <div class="widget-description">
+                                    <p class="mt-4">There are many variations of passages of Lorem Ipsum available, but
+                                        the
+                                        majority have
+                                        suffered alteration in some form, by injected humour, or randomised words which
+                                        don't
+                                        look even slightly believable. If you are going to use a passage of Lorem Ipsum,
+                                        you
+                                        need to be sure there isn't anything embarrassing hidden in the middle of text.
+                                    </p>
+                                    <a class="btn btn-outline-light" href="#" role="button">Edit Section</a>
+
+                                </div>
+
+                            </div>
+                            <div class="widget-2">
+                                <h1 class="border-bottom border-top">Heartset</h1>
+                                <div class="edit-widget-btn">
+                                    <a href="#"> <span><i class="far fa-edit"></i></span> </a>
+                                </div>
+
+                                <p class="mt-4">There are many variations of passages of Lorem Ipsum available, but the
+                                    majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which
+                                    don't
+                                    look even slightly believable. If you are going to use a passage of Lorem Ipsum, you
+                                    need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+                                <a class="btn btn-outline-light" href="#" role="button">Edit Section</a>
+                            </div>
+                            <div class="widget-3">
+                                <h1 class="border-bottom border-top">Soulet</h1>
+                                <div class="edit-widget-btn">
+                                    <a href="#"> <span><i class="far fa-edit"></i></span> </a>
+                                </div>
+
+                                <p class="mt-4">There are many variations of passages of Lorem Ipsum available, but the
+                                    majority have
+                                    suffered alteration in some form, by injected humour, or randomised words which
+                                    don't
+                                    look even slightly believable. If you are going to use a passage of Lorem Ipsum, you
+                                    need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+                                <a class="btn btn-outline-light" href="#" role="button">Edit Section</a>
+                            </div>
+
+                            <div class="left-nav-footer-section margin-top-3 text-center  border-top">
+                                <a href="#"><span>About</span></a>
+                                <a href="#"><span>Terms &amp; Condition</span></a>
+                                <a href="#"><span>Contact</span></a>
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
     </div>
-   
-    
-  </div>
-</div>
-
-   </div>
-
- 
-   </div>
-
-
-<div class="container">
-<div class="profile-card-top-section">    
-<div class="row">
-<div class="user-profile-avatar d-flex justify-content-center">
-<div class="profile-img-src" style="background-image: url(fucking-images/dev-chatterjee-seo-expert.jpg);"></div>
-<!-- <img src="fucking-images/dev-chatterjee-seo-expert.jpg"  class="img-fluid img-thumbnail" alt="..."> -->
-
-</div>
-</div>
-
-<div class="user-top-information">
-    <div class="row">
-        <div class="col-md-4 user-top-left-side-widget border-end">
-          <h3>My Money, My Income</h3>
-          <h5>Inbound Medic <small>(Founder)</small></h5> 
-          <a href="www.inboundmedic.com" target="_blank"><i class="fas fa-external-link-alt"></i> www.inboundmedic.com</a>
-          <p>I help elite medical practices get more new patients through Google.</p>
-          <a class="btn btn-primary user-top-left-side-widget-btn" href="#" role="button">Schedule A Strategy Session</a>
-        </div>
-
-        <div class="col-md-4 user-top-center-side-widget border-end text-center">
-            <h3>Dev Chatterjee</h3>
-            <span>Artist</span> | <span>Entrepreneur</span> | Fool
-Creating The Next Social Network
-And Pissing Off The Entire World
-        </div>
-
-        <div class="col-md-4">
-            asdasdad
-        </div>
-
-        </div>
-
-
-        </div>
-    </div>
-
-</div>
-
-
-
-
+    </section>
 
 
 
