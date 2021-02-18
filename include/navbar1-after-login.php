2a3934793751eb544eb3dfa7ea04c4986eeddb41
dreamlineproduction <info@dreamlineproduction.in>
2021-02-18 17:07:21 +0530

    Userprofile nav bar added (Desktop)

--- include/navbar1-after-login.php
+++ include/navbar1-after-login.php
@@ -1,38 +1,71 @@
-<nav class="navbar navbar-expand-lg  bg-black text-white">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="fucking-images/imagn_logo.png">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+<nav class="bg-black text-white p-3">
+    <div class="container">
 
-            <div class="d-flex justify-content-start  d-none d-sm-block"> <span class="navbar-text">
-            A World Without Borders
-        </span></div>
-            <div class="d-flex justify-content-end">
+        <div class="row">
+            <div class="col-md-2">
+                <a class="navbar-brand" href="index.php">
+                    <img src="fucking-images/imagn_logo.png">
+                </a>
+            </div>
 
-            <div class="dropdown">
+            <div class="col-md-4 d-flex align-items-center">
+                <div class="d-none d-sm-block">
+                    <span class="navbar-text">
+                        A World Without Borders
+                    </span>
 
-  
-                        <img class="user-drop" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" src="fucking-images/dev-chatterjee-seo-expert.jpg"  class="img-fluid img-thumbnail" alt="...">
 
-                
+                </div>
 
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <li><a class="dropdown-item" href="#">My Profile</a></li>
-    <li><a class="dropdown-item" href="#">Account Settings</a></li>
-    <li><a class="dropdown-item" href="#">Help</a></li>
-    <li><a class="dropdown-item" href="#">Logout</a></li>
-  </ul>
-</div>
+
 
             </div>
 
+            <div class="col-md-5">
+                <div class="text-end">
+
+                    <a class="btn btn-primary dreem-feed-btn" href="#" role="button">Dream Feed</a>
+                    <a class="toggle-icon" href="#">
+                        <i class="far fa-comment"></i>
+                    </a>
+                    <a class="toggle-icon" href="#">
+                        <i class="far fa-bell"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-1 user-drop-menu">
+                <div class="dropdown">
+
+
+                    <img class="user-drop" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                        aria-expanded="false" src="fucking-images/dev-chatterjee-seo-expert.jpg"
+                        class="img-fluid img-thumbnail" alt="...">
 
 
 
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="user-profile.php">My Profile</a></li>
+                        <li><a class="dropdown-item" href="account-setting.php">Account Settings</a></li>
+                        <li><a class="dropdown-item" href="help.php">Help</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </div>
+
+
+            </div>
 
         </div>
-    </nav>
 
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</nav>
